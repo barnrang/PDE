ee380971f3896e31963af97138575615a345471c
barnrang <barnrang@gmail.com>
2017-11-06 11:10:20 +0900

    add neumann

--- lec2/real.py
+++ lec2/real.py
@@ -6,24 +6,27 @@ os.chdir(dir)
 
 dx = 0.1
 dy = dx
-dt = 0.6
+dt = 0.1
 alpha = 1.
 grid_x = 100
 grid_y = 100
 nt = 100
-d = 1
+d = 0.1
 dt = d * (dx**2)/alpha
 
 x = np.linspace(0,dx * (grid_x - 1), grid_x)
 y = np.linspace(0,dy * (grid_y - 1), grid_y)
 X,Y = np.meshgrid(x,y)
 
+
+X_set = (40/grid_x * np.arange(0,grid_x))**2
+Y_set = (40/grid_y * np.arange(0,grid_y))**2
 cmap = plt.cm.get_cmap("jet")
 cmap.set_over('grey')
 T = np.zeros((grid_x,grid_y))
+T += np.sqrt(X_set.reshape((grid_x,1)) + Y_set.reshape((1,grid_y)))
 g = 0
-T[40:60,40:60] = 50.
-levels = np.arange(0.,100.,0.2)
+levels = np.arange(0.,60.,0.2)
 icount = 1
 ###for n in range(1,100):
 ###	plt.cla()
@@ -43,12 +46,20 @@ for n in range(1,100):
 	plt.cla()
 	plt.clf()
 	Tn = T.copy()
-	T = Tn+d*(np.roll(Tn,1,axis=0)+np.roll(Tn,-1,axis=0)+np.roll(Tn,1,axis=1)+np.roll(Tn,-1,axis=1)-4*Tn)
+	left = np.roll(Tn,1,axis=0)
+	right = np.roll(Tn,-1,axis=0)
+	up = np.roll(Tn,-1,axis=1)
+	down = np.roll(Tn,1,axis=1)
+	left[0,:],right[grid_x-1,:],up[:,0],down[:,grid_y-1] = 0,0,0,0
+	T = Tn+d*(up+down+right+left-4*Tn)
+	T[0,1:grid_y-1] = d * Tn[1,1:grid_y-1]
+	T[grid_x-1,1:grid_y-1] += d * Tn[grid_x-1,1:grid_y-1]
+	T[1:grid_x-1,0] += d * Tn[1:grid_x-1,1]
+	T[1:grid_x-1,grid_y-1] += d * Tn[1:grid_x-1,grid_y-2]
 	plt.xlim(0.,np.max(x))
 	plt.ylim(0.,np.max(x))
 	cl = plt.contourf(X,Y,T,levels,cmap=cmap)
 	plt.colorbar(cl)
 	plt.text(np.max(x)*0.8,np.max(y)+dy,"t=%01.5f"%(dt*n))
 	plt.savefig('cyclic_%04i.jpg'%(icount))
-	T[0,:],T[:,0],T[grid_x-1,:],T[:,grid_y-1] = T[1,:]+g*dx*2,T[:,1]+g*dx*2,T[grid_x-2,:]+g*dx*2,T[:,grid_y-2]+g*dx*2 
 	icount += 1
